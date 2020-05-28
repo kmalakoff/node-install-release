5e1d59f122612060adcc0a0c2e1f3df885626a92
Kevin Malakoff <kmalakoff@gmail.com>
2020-05-27 22:48:19 -0700

    Update modules

--- test/spec/install-release.test.js
+++ test/spec/install-release.test.js
@@ -13,8 +13,7 @@ var OPTIONS = {
   buildDirectory: path.join(TMP_DIR, 'build'),
 };
 var VERSIONS = ['v14', 'v12', 'v10', 'v8', 'v6', 'v4', 'v0.10', 'v0.8', 'v0.6'];
-// var TARGETS = [{ platform: 'darwin', arch: 'x64' }, { platform: 'linux', arch: 'x64' }, { platform: 'win32', arch: 'x64' }, {}];
-var TARGETS = [{}];
+var TARGETS = [{ platform: 'darwin', arch: 'x64' }, { platform: 'linux', arch: 'x64' }, { platform: 'win32', arch: 'x64' }, {}];
 
 function addTests(version, target) {
   var platform = target.platform || 'local';
