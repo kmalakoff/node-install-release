ce3781c12d202081eb6a9c7ac6423142e0a59849
Kevin Malakoff <kmalakoff@gmail.com>
2020-06-04 18:54:50 -0700

    Refactor for multiple installs simultaneously

--- index.js
+++ index.js
@@ -1,15 +1,15 @@
 var install = require('./lib/install');
 
-function installRelease(versionString, dest, options, callback) {
+function installRelease(versionDetails, dest, options, callback) {
   if (typeof options === 'function') {
     callback = options;
     options = null;
   }
   options = options || {};
 
-  if (typeof callback === 'function') return install(versionString, dest, options, callback);
+  if (typeof callback === 'function') return install(versionDetails, dest, options, callback);
   return new Promise(function (resolve, reject) {
-    installRelease(versionString, dest, options, function installCallback(err, result) {
+    installRelease(versionDetails, dest, options, function installCallback(err, result) {
       err ? reject(err) : resolve(result);
     });
   });
