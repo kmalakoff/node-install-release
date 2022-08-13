32b6cf469e5ccc1f34e4ea46ba8f16a6eadf235b
Kevin Malakoff <kmalakoff@gmail.com>
2022-08-13 07:54:25 -0700

    Simplify npm version lookup

--- test/spec/install-release.test.js
+++ test/spec/install-release.test.js
@@ -12,18 +12,21 @@ var OPTIONS = {
   cacheDirectory: path.join(TMP_DIR, 'cache'),
   buildDirectory: path.join(TMP_DIR, 'build'),
 };
-var VERSIONS = resolveVersions.sync('>=0.8', { range: 'major,even' })
+var VERSIONS = resolveVersions.sync('>=0.8', { range: 'major,even' });
 // VERSIONS = ['v4'];
+// VERSIONS = ['v10'];
+
 var TARGETS = [{ platform: 'darwin', arch: 'x64' }, { platform: 'linux', arch: 'x64' }, { platform: 'win32', arch: 'x64' }, {}];
 // TARGETS = [{}];
 // TARGETS = [{ platform: 'win32', arch: 'x64' }];
+TARGETS = [{ platform: 'darwin', arch: 'x64' }];
 
 function addTests(version, target) {
   var platform = target.platform || 'local';
   var arch = target.arch || 'local';
 
   describe(version + ' (' + platform + ',' + arch + ')', function () {
-    it.only('dist', function (done) {
+    it('dist', function (done) {
       var installPath = path.join(INSTALLED_DIR, version + '-' + platform + '-' + arch);
       installRelease(version, installPath, Object.assign({}, target, OPTIONS), function (err) {
         assert.ok(!err);
@@ -39,13 +42,13 @@ function addTests(version, target) {
         })
         .catch(done);
     });
-  });
 
-  it.skip('source', function (done) {
-    var installPath = path.join(INSTALLED_DIR, version + '-' + platform + '-' + arch + '-src');
-    installRelease(version, installPath, Object.assign({ filename: 'src' }, OPTIONS), function (err, res) {
-      assert.ok(!err);
-      validateInstall(version, installPath);
+    it.skip('source', function (done) {
+      var installPath = path.join(INSTALLED_DIR, version + '-' + platform + '-' + arch + '-src');
+      installRelease(version, installPath, Object.assign({ filename: 'src' }, OPTIONS), function (err, res) {
+        assert.ok(!err);
+        validateInstall(version, installPath, done);
+      });
     });
   });
 }
@@ -57,14 +60,9 @@ describe('install-release', function () {
     });
   });
 
-  describe('happy path', function () {
-    for (var i = 0; i < VERSIONS.length; i++) {
-      for (var j = 0; j < TARGETS.length; j++) {
-        addTests(VERSIONS[i], TARGETS[j]);
-      }
+  for (var i = 0; i < VERSIONS.length; i++) {
+    for (var j = 0; j < TARGETS.length; j++) {
+      addTests(VERSIONS[i], TARGETS[j]);
     }
-  });
-
-  // TODO
-  describe('unhappy path', function () {});
+  }
 });
