dbde8bccfbbbb78893a1bc615beed181b1ce4aca
Kevin Malakoff <kmalakoff@gmail.com>
2024-12-10 18:09:29 -0800

    Upgrade modules

--- dist/cjs/installNPM/index.js
+++ dist/cjs/installNPM/index.js
@@ -7,4 +7,4 @@ module.exports = function install(version, dest, options, callback) {
         installBin(version, dest, options, callback);
     });
 };
-/* CJS INTEROP */ if (exports.__esModule && exports.default) { Object.defineProperty(exports.default, '__esModule', { value: true }); for (var key in exports) exports.default[key] = exports[key]; module.exports = exports.default; }
+/* CJS INTEROP */ if (exports.__esModule && exports.default) { try { Object.defineProperty(exports.default, '__esModule', { value: true }); for (var key in exports) { exports.default[key] = exports[key]; } } catch (_) {}; module.exports = exports.default; }
--- dist/cjs/installNode/installSource/buildWin32.js
+++ dist/cjs/installNode/installSource/buildWin32.js
@@ -23,4 +23,4 @@ module.exports = function installWin32(buildPath, dest, _options, callback) {
         queue.await(callback);
     });
 };
-/* CJS INTEROP */ if (exports.__esModule && exports.default) { Object.defineProperty(exports.default, '__esModule', { value: true }); for (var key in exports) exports.default[key] = exports[key]; module.exports = exports.default; }
+/* CJS INTEROP */ if (exports.__esModule && exports.default) { try { Object.defineProperty(exports.default, '__esModule', { value: true }); for (var key in exports) { exports.default[key] = exports[key]; } } catch (_) {}; module.exports = exports.default; }
