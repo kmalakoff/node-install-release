b9f4836f9c88dd4a17c81b7979696c0e938dd623
Kevin Malakoff <kmalakoff@gmail.com>
2024-12-30 01:16:20 -0800

    Upgrade modules

--- src/installNode/findDistPaths.js
+++ src/installNode/findDistPaths.js
@@ -5,7 +5,7 @@ module.exports = function findDistPaths(version, options) {
   const filenames = options.filename ? [options.filename] : prebuilds(options);
   for (let index = 0; index < filenames.length; index++) {
     const filename = filenames[index];
-    if (!~version.files.indexOf(filename)) continue;
+    if (version.files.indexOf(filename) < 0) continue;
     const relativePaths = distPaths(filename, version.version);
     if (relativePaths && relativePaths.length) return { version: version.version, filename: filename, relativePaths: relativePaths };
   }
