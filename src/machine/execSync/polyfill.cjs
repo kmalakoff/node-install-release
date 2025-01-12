63c206213fb9e6c1e1bf4566439b853bf084318b
Kevin Malakoff <kmalakoff@gmail.com>
2025-01-12 12:40:44 -0800

    refactor: rename nir to install and update test logic

--- src/machine/execSync/polyfill.cjs
+++ src/machine/execSync/polyfill.cjs
@@ -1,11 +1,11 @@
 const cp = require('child_process');
 if (!cp.execSync) {
   const path = require('path');
-  const execCallback = path.join(__dirname, 'worker.cjs');
+  const workerPath = path.join(__dirname, 'worker.cjs');
 
   let functionExec = null; // break dependencies
   cp.execSync = function execSyncPolyfill(cmd, options = {}) {
     if (!functionExec) functionExec = require('function-exec-sync');
-    return functionExec({ callbacks: true }, execCallback, cmd, options);
+    return functionExec({ callbacks: true }, workerPath, cmd, options);
   };
 }
