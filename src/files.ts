e36900126db10d920e838f6feaa5d5a2bf52c446
Kevin Malakoff <kmalakoff@gmail.com>
2025-06-21 00:47:37 -0700

    refactor: move file assets into src and update dependencies

---
+++ src/files.ts
@@ -0,0 +1,5 @@
+import Module from 'module';
+
+const _require = typeof require === 'undefined' ? Module.createRequire(import.meta.url) : require;
+const path = '../../assets/files.cjs';
+export default _require(path);
