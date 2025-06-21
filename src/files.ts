493a705c7fbe494b0fe9b24bbb79557b6b4e206c
Kevin Malakoff <kmalakoff@gmail.com>
2025-06-21 08:50:04 -0700

    rewriteRelativeImportExtensions

--- src/files.ts
+++ src/files.ts
@@ -2,4 +2,4 @@ import Module from 'module';
 
 const _require = typeof require === 'undefined' ? Module.createRequire(import.meta.url) : require;
 const path = '../../assets/files.cjs';
-export default _require(path);
+export default _require(path) as JSON;
