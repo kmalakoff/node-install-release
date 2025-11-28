2772482b00aaebe30bb346e4b428c547ea316a7c
Kevin Malakoff <kmalakoff@gmail.com>
2025-11-28 11:10:35 -0800

    Upgrade to cpu-arch

--- src/lib/getTarget.ts
+++ src/lib/getTarget.ts
@@ -1,9 +1,9 @@
+import cpuArch from 'cpu-arch';
 import { FILE_PLATFORM_MAP, FILE_PLATFORMS } from '../constants.ts';
-import machineArch from '../machine/arch.cjs';
-
-let archMachine: NodeJS.Architecture;
 
 import type { InstallOptions, Target } from '../types.ts';
+
+const archCPU = cpuArch();
 
 export default function getTarget(options: InstallOptions): Target {
   let platform = options.platform;
@@ -14,9 +14,6 @@ export default function getTarget(options: InstallOptions): Target {
     arch = options.filename.split('-')[1] as NodeJS.Architecture;
   }
   if (!platform) platform = process.platform;
-  if (!arch) {
-    if (!archMachine) archMachine = machineArch() as NodeJS.Architecture; // call first time needed
-    arch = archMachine;
-  }
+  if (!arch) arch = archCPU as NodeJS.Architecture;
   return { platform, arch };
 }
