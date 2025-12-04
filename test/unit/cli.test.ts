dcab19e758d30ece8c01483cb0686dc4a96d08c2
Kevin Malakoff <kmalakoff@gmail.com>
2025-12-04 15:27:18 -0800

    replace rimraf2 with fs-remove-compat

--- test/unit/cli.test.ts
+++ test/unit/cli.test.ts
@@ -4,9 +4,9 @@ delete process.env.NODE_OPTIONS;
 import assert from 'assert';
 import cr from 'cr';
 import fs from 'fs';
+import { safeRm } from 'fs-remove-compat';
 import isVersion from 'is-version';
 import path from 'path';
-import rimraf2 from 'rimraf2';
 import url from 'url';
 
 const isWindows = process.platform === 'win32' || /^(msys|cygwin)$/.test(process.env.OSTYPE);
@@ -82,8 +82,8 @@ function addTests(version) {
 }
 
 describe('cli', () => {
-  before(rimraf2.bind(null, TMP_DIR, { disableGlob: true }));
-  after(rimraf2.bind(null, TMP_DIR, { disableGlob: true }));
+  before((cb) => safeRm(TMP_DIR, cb));
+  after((cb) => safeRm(TMP_DIR, cb));
 
   describe('--version', () => {
     it('outputs version with --version', (done) => {
