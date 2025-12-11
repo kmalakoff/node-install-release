4cef07d4e83304b7e9084b28bc64e01d3d0b50cb
Kevin Malakoff <kmalakoff@gmail.com>
2025-12-11 13:59:41 -0800

    Split npm existence check from node. Use bundled npm as long as version >= 3

--- src/installNode/installCompressed.ts
+++ src/installNode/installCompressed.ts
@@ -1,5 +1,4 @@
 import fs from 'fs';
-import { safeRm } from 'fs-remove-compat';
 import path from 'path';
 import Queue from 'queue-cb';
 
@@ -10,7 +9,6 @@ import type { ChecksumCallback, ChecksumResult, InstallOptions } from '../types.
 import validateDownload from './validateDownload.ts';
 
 export default function installCompressed(distPath: string, dest: string, options: InstallOptions, callback: ChecksumCallback): undefined {
-  const platform = options.platform;
   const downloadPath = `${NODE_DIST_BASE_URL}/${distPath}`;
   const cachePath = path.join(options.cachePath, path.basename(downloadPath));
 
@@ -27,11 +25,6 @@ export default function installCompressed(distPath: string, dest: string, option
       });
     });
     queue.defer(conditionalExtract.bind(null, cachePath, dest, { strip: 1, time: 1000 }));
-    queue.await((err) => {
-      // some compressed versions of node come with npm pre-installed, but we want to override with a specific version
-      const libPath = platform === 'win32' ? dest : path.join(dest, 'lib');
-      const installPath = path.join(libPath, 'node_modules', 'npm');
-      safeRm(installPath, () => callback(err, checksum));
-    });
+    queue.await((err) => callback(err, checksum));
   });
 }
