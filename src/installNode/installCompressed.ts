ccfb7c254e4ac429626f2abc0294a84508084455
Kevin Malakoff <kmalakoff@gmail.com>
2025-12-12 10:45:13 -0800

    Atomic operations to gurantee only successful installs occur using atomic operations

--- src/installNode/installCompressed.ts
+++ src/installNode/installCompressed.ts
@@ -1,10 +1,9 @@
-import fs from 'fs';
 import path from 'path';
 import Queue from 'queue-cb';
 
 import { NODE_DIST_BASE_URL } from '../constants.ts';
 import conditionalCache from '../lib/conditionalCache.ts';
-import conditionalExtract from '../lib/conditionalExtract.ts';
+import extract from '../lib/extract.ts';
 import type { ChecksumCallback, ChecksumResult, InstallOptions } from '../types.ts';
 import validateDownload from './validateDownload.ts';
 
@@ -12,19 +11,15 @@ export default function installCompressed(distPath: string, dest: string, option
   const downloadPath = `${NODE_DIST_BASE_URL}/${distPath}`;
   const cachePath = path.join(options.cachePath, path.basename(downloadPath));
 
-  fs.stat(dest, (err) => {
-    if (!err) return callback(); // already exists
-
-    let checksum: ChecksumResult | undefined;
-    const queue = new Queue(1);
-    queue.defer(conditionalCache.bind(null, downloadPath, cachePath));
-    queue.defer((cb) => {
-      validateDownload(distPath, cachePath, (err, result) => {
-        checksum = result;
-        cb(err);
-      });
+  let checksum: ChecksumResult | undefined;
+  const queue = new Queue(1);
+  queue.defer(conditionalCache.bind(null, downloadPath, cachePath));
+  queue.defer((cb) => {
+    validateDownload(distPath, cachePath, (err, result) => {
+      checksum = result;
+      cb(err);
     });
-    queue.defer(conditionalExtract.bind(null, cachePath, dest, { strip: 1, time: 1000 }));
-    queue.await((err) => callback(err, checksum));
   });
+  queue.defer(extract.bind(null, cachePath, dest, { strip: 1, time: 1000 }));
+  queue.await((err) => callback(err, checksum));
 }
